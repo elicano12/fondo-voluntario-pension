d625edeebe7f5fe7651d387068f3ff444b73adcd
Eli Cano <ele.cano111@gmail.com>
2024-10-07 17:43:05 -0500

    Fixed some word to file

--- api/src/services/transaction.services.js
+++ api/src/services/transaction.services.js
@@ -7,18 +7,18 @@ const { enviarSMS, enviarEmails } = require("../utils");
 const { NotFoundError, BadRequestError } = require("../utils/errorHandler");
 
 const getTransactions = async () => {
-  const transacciones = await transactionsRepository.getTransactions();
+  const transactions = await transactionsRepository.getTransactions();
 
-  if (transacciones.length === 0) {
-    throw new NotFoundError("transacciones not found");
+  if (transactions.length === 0) {
+    throw new NotFoundError("transactions not found");
   }
 
-  return transacciones;
+  return transactions;
 };
 
 const getTransactionUserId = async (id, tipo) => {
   if (!id) {
-    throw new BadRequestError("El campo id es requerido");
+    throw new BadRequestError("Missing required fields: id");
   }
 
   const transacciones = await transactionsRepository.getTransactionUserId(
@@ -29,116 +29,97 @@ const getTransactionUserId = async (id, tipo) => {
 };
 
 const postOpenFund = async (usuarioId, fondoId, monto, tipo) => {
-    if (!usuarioId || !fondoId || !monto) {
-      throw new BadRequestError(
-        "Faltan campos requeridos: usuarioId, fondoId y monto"
-      );
-    }
+  if (!usuarioId || !fondoId || !monto) {
+    throw new BadRequestError(
+      "Missing required fields: usuarioId, fondoId y monto"
+    );
+  }
 
-    const usuario = await usersRepository.getUserById(usuarioId);
-    const fondo = await fundRepository.getFundPensionsById(fondoId);
+  const users = await usersRepository.getUserById(usuarioId);
+  const fund = await fundRepository.getFundPensionsById(fundId);
 
-    if (usuario == null || fondo == null) {
-      throw new NotFoundError("Usuario o fondo no encontrados");
-    }
+  if (users == null || fund == null) {
+    throw new NotFoundError("Users o Funds not found");
+  }
 
-    if (usuario.saldo < monto) {
-      throw new NotFoundError(
-        `No tiene saldo disponible para vincularse al fondo ${fondo.nombre}`
-      );
-    }
+  if (users.saldo < monto) {
+    throw new NotFoundError(
+      `No tiene saldo disponible para vincularse al fondo ${fund.nombre}`
+    );
+  }
 
-    if (fondo.montoMinimo > monto) {
-      throw new BadRequestError(
-        `El fondo ${fondo.nombre} no cumple con el monto mínimo de ${fondo.montoMinimo}`
-      );
-    }
+  if (fund.montoMinimo > monto) {
+    throw new BadRequestError(
+      `El fondo ${fondo.nombre} no cumple con el monto mínimo de ${fund.montoMinimo}`
+    );
+  }
 
-    usuario.saldo -= monto;
+  users.saldo -= monto;
 
-    await usersRepository.saveUser(usuario);
+  await usersRepository.saveUser(users);
 
-    const transaccion = {
-      usuarioId,
-      fondoId,
-      monto,
-      tipo,
-    };
+  const trsnsaction = {
+    usuarioId,
+    fondoId,
+    monto,
+    tipo,
+  };
 
-    await transactionsRepository.saveTransaction(transaccion);
+  await transactionsRepository.saveTransaction(trsnsaction);
 
-    if (usuario.notificaciones === "email") {
-      enviarEmails(
-        usuario.email,
-        `Suscripción exitosa al fondo ${fondo.nombre}`
-      );
-    } else {
-      enviarSMS(
-        usuario.telefono,
-        `Suscripción exitosa al fondo ${fondo.nombre}`
-      );
-    }
+  if (users.notificaciones === "email") {
+    enviarEmails(users.email, `Suscripción exitosa al fondo ${fund.nombre}`);
+  } else {
+    enviarSMS(users.telefono, `Suscripción exitosa al fondo ${fund.nombre}`);
+  }
 
-    return { fondo, message: "Suscripción realizada con éxito." };
+  return { fund, message: "Suscripción realizada con éxito." };
 };
 
 const postCancelFund = async (usuarioId, fondoId) => {
-  try {
-    if (!usuarioId || !fondoId) {
-      throw new BadRequestError(
-        "Faltan campos requeridos: usuarioId, fondoId "
-      );
-    }
-
-    const fondo = await fundRepository.getFundPensionsById(fondoId);
-    const usuario = await usersRepository.getUserById(usuarioId);
-
-    if (fondo == null || usuario == null) {
-      throw new NotFoundError("Usuario o Fondo no encontrado");
-    }
-
-    const transaccion = await transactionsRepository.getCancelTransaction(
-      usuarioId,
-      fondoId
-    );
-
-    if (!transaccion) {
-      throw new NotFoundError(
-        `No se encontró la transacción activa al fondo ${fondo.nombre}`
-      );
-    }
-
-    usuario.saldo += transaccion.monto;
-
-    await usersRepository.saveUser(usuario);
-
-    const transaccionCancelar = {
-      _id: transaccion._id,
-      usuarioId,
-      fondoId,
-      tipo: "cancelado",
-      monto: transaccion.monto,
-    };
-
-    await transactionsRepository.updateTransactions(transaccionCancelar);
-
-    // if (usuario.notificaciones === "email") {
-    //   enviarEmails(
-    //     usuario.email,
-    //     `Suscripción exitosa al fondo ${fondo.nombre}`
-    //   );
-    // } else {
-    //   enviarSMS(
-    //     usuario.telefono,
-    //     `Suscripción exitosa al fondo ${fondo.nombre}`
-    //   );
-    // }
-
-    return { fondo, message: "Cancelación realizada con éxito." };
-  } catch (error) {
-    console.error(error);
-    throw new Error(error.message);
+  if (!usuarioId || !fondoId) {
+    throw new BadRequestError("Missing required fields: usuarioId, fondoId ");
   }
+
+  const fund = await fundRepository.getFundPensionsById(fondoId);
+  const users = await usersRepository.getUserById(usuarioId);
+
+  if (fund == null || users == null) {
+    throw new NotFoundError("Users o Funds not found");
+  }
+
+  const transaction = await transactionsRepository.getCancelTransaction(
+    usuarioId,
+    fondoId
+  );
+
+  if (!transaction) {
+    throw new NotFoundError(
+      `No se encontró la transacción activa al fondo ${fondo.nombre}`
+    );
+  }
+
+  users.saldo += transaction.monto;
+
+  await usersRepository.saveUser(users);
+
+  const transactionCancelar = {
+    _id: transaction._id,
+    usuarioId,
+    fondoId,
+    tipo: "cancelado",
+    monto: transaction.monto,
+  };
+
+  await transactionsRepository.updateTransactions(transactionCancelar);
+
+  if (users.notificaciones === "email") {
+    enviarEmails(users.email, `Suscripción exitosa al fondo ${fund.nombre}`);
+  } else {
+    enviarSMS(users.telefono, `Suscripción exitosa al fondo ${fund.nombre}`);
+  }
+
+  return { fund, message: "Cancelación realizada con éxito." };
 };
 
 module.exports = {
